8724c81f7f7054fa2c0a2b6b509bcae0cae4febb
Ki-Sung <kcs4912@gmail.com>
2022-07-25 15:19:21 +0900

    setting to login event

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,19 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+// const { login } = require("../../../routes/home/home.ctrl");
+
+const id = document.querySelector("#id"),
+password = document.querySelector("#password"),
+loginBtn = document.querySelector("button");
+
+// 클릭이라는 이벤트를 발생시키면 로그인하기 
+loginBtn.addEventListener("click", login);
+
+// 위 두 번쨰 파라미터 login 함수 설정 
+function login() {
+    const req = {
+        id: id.value,
+        password: password.value,
+    };
+    console.log(req);
+}
