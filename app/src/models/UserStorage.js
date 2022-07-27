021436497fe51f7cc65925f546745a93b1f7910d
Ki-Sung <kcs4912@gmail.com>
2022-07-27 21:43:00 +0900

    make userstorage model

---
+++ app/src/models/UserStorage.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const { use } = require("../routes/home");
+
+// 클래스를 만들 떄 파일이름과 동일하게 하면 좋다. 
+class UserStorage {
+    static #users = {
+        id: ["kcs4912" , "flycode77", "jihyun"],
+        password: ["1234", "12345", "123456"],
+        name: ["김기성", "정현석", "김지현"],
+    };
+
+    // 외부에서 로그인 기능을 위한 로그인 로직처리를 위한 메서드
+    static getUsers(...fields) {
+        const users = this.#users;
+        const newUsers = fields.reduce((newUsers, field) => {
+            if (users.hasOwnProperty(field)) {
+                newUsers[field] = users[field];
+            }
+            return newUsers;
+        }, {});
+        return newUsers;
+    }
+
+}
+
+module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,10 +1,8 @@
 // 자바스크립트 파일 생성 시 맨 상단에 이크마스크립트를 준수하겠다는 내용을 기재한다. 꼭!
 "use strict";
 
-const users = {
-    id: ["kcs4912" , "flycode77", "jihyun"],
-    password: ["1234", "12345", "123456"]
-};
+// UserStorage 모델 불러오기 
+const UserStorage = require("../../models/UserStorage")
 
 const output = {
     // home 컨트롤러
@@ -23,24 +21,25 @@ const process = {
         // id와 password 프론트에서 받아오기
         const id = req.body.id,
         password = req.body.password;
-        
+    
+    // 클래스 인스턴스를 만들어 줌 
+    const users = UserStorage.getUsers("id", "password");
         // id, password 검증하기 
         // 프론트엔드에서 전달한 id가 user의 id에 있으면, users에 indexOF를 사용해서 id 인덱스를 가져오도록 하기
+        const response = {};
         if (users.id.includes(id)) {
             const idx = users.id.indexOf(id);
             // user.password[idx]가 프론트엔드에서 전달한 password와 같은지 여부 확인
             if (users.password[idx] === password) {
                 // 만약 같으면 성공여부 응답
-                return res.json({
-                    success: true,
-                }); 
+                response.success = true;
+                return res.json(response); 
             }
         }
-        // 실패 했다면 실패로 나타내주기 
-        return res.json({
-            success: false,
-            msg: "아이디 또는 비밀번호를 잘못 입력했습니다. 다시 확인해주세요."
-        });
+        // // 실패 했다면 실패로 나타내주기 
+        response.success = false;
+        response.msg =  "아이디 또는 비밀번호를 잘못 입력했습니다. 다시 확인해주세요."
+        return res.json(response);
     },
 };
 
